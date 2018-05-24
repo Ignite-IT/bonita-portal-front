3be4ebccc219760e66d882ef428e82ee02453f6f
Eduardo Nola <edunola13@hotmail.com>
2018-05-24 11:55:15 -0300

    Archivos de Configuracion

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   //API
-  //apiHost: "http://localhost/bonita-portal-api/",
-  apiHost: "http://200.41.233.251:4423/bonita-portal-api/",
+  apiHost: "http://localhost/bonita-portal-api/",
+//  apiHost: "http://200.41.233.251:4423/bonita-portal-api/",
   
   defaultPage: '/tasks',
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   //API
   apiHost: "http://localhost/bonita-portal-api/",
   //apiHost: "http://200.41.233.251:4423/bonita-portal-api/",
