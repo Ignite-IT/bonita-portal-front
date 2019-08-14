a85db254d5d7ce10d3f14bdd7ed2b2c830d0bd65
jmgarro <jmgarro@aknotec.com.ar>
2019-08-14 13:28:34 -0300

    gsdfg

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,20 @@ export const environment = {
   production: true,
   //API
 //  apiHost: "http://localhost/bonita-portal-api/",
-  apiHost: "http://200.41.233.251:4423/bonita-portal-api/",
+//  es la url de test del usuario
+apiHost: "http://200.41.233.251:4423/bonita-portal-api/",
+//  es la url de test nuestra
+//apiHost: "http://200.41.233.251:4433/bonita-portal-api/",
   
-  //apiHostCaja: "http://localhost/caja-forms-api/",
+//  apiHostCaja: "http://localhost/caja-forms-api/",
+//  es la url de test del usuario
   apiHostCaja: "http://200.41.233.251:4423/caja-forms-api/",
-    
-  apiHostMedicos: "http://200.41.233.251:4423/caja-forms-api/cpsmProxy/",
+//	es la url de test nuestra
+//apiHostCaja: "http://200.41.233.251:4433/caja-forms-api/",
+
+// es la url de test del usuario    
+apiHostMedicos: "http://200.41.233.251:4423/caja-forms-api/cpsmProxy/",
+//apiHostMedicos: "http://200.41.233.251:4433/caja-forms-api/cpsmProxy/",
   
   defaultPage: '/tasks',
   
